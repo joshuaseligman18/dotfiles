aaa3bcfd3ba9a30dfa40cbce33363ef6db290410
Joshua Seligman <joshuaseligman18@gmail.com>
2024-10-24 13:37:44 -0400

    Added folding to nvim config

--- .config/nvim/lua/myconfig/plugins/treesitter.lua
+++ .config/nvim/lua/myconfig/plugins/treesitter.lua
@@ -20,7 +20,9 @@ return {
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
             auto_install = true,
-
+            indent = {
+                enable = true,
+            },
             highlight = {
                 enable = true,
 

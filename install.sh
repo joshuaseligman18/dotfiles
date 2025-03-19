e77f932ee6d208c6eef0846355f7171c69685296
Joshua Seligman <joshuaseligman18@gmail.com>
2025-03-18 22:26:17 -0400

    Added nvim config and added an install script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+brew update
+brew doctor
+
+brew install antigen
+brew install fd
+brew install ripgrep
+brew install neovim
+brew install starship
+brew install stow
+
+brew install --cask aerospace
+brew install --cask ghostty

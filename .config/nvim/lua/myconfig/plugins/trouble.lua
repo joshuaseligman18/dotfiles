881d6007e8237d0da8fc356eca249c84cbd68a02
Joshua Seligman <joshuaseligman18@gmail.com>
2025-03-24 21:49:34 -0400

    Lsp keybinding fixes and added trouble plugin

---
+++ .config/nvim/lua/myconfig/plugins/trouble.lua
@@ -0,0 +1,12 @@
+return {
+    "folke/trouble.nvim",
+    opts = {},
+    cmd = "Trouble",
+    keys = {
+        {
+            "<leader>tt",
+            "<cmd>Trouble diagnostics toggle focus=true<cr>",
+            desc = "Trouble Diagnostics",
+        },
+    },
+}
